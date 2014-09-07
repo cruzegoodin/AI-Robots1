93bc863579c29e2b955f401fd3cb073bd3dbea8f
cruzegoodin <cruzegoodin@yahoo.com>
2014-09-07 18:58:37 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,17 @@ import lejos.robotics.RegulatedMotor;
 public class Main {
 
 	public static void main(String[] args) {
-		RegulatedMotor a = new EV3LargeRegulatedMotor(MotorPort.A);
 		
+		RegulatedMotor a = new EV3LargeRegulatedMotor(MotorPort.A);
 		RegulatedMotor b = new EV3LargeRegulatedMotor(MotorPort.B);
+		RegulatedMotor c = new EV3LargeRegulatedMotor(MotorPort.C);
+		
 		b.setSpeed(80);
 		b.rotate(310);
 		
 		a.rotate(70);
 		
 		b.rotate(-310);
-		
-		RegulatedMotor c = new EV3LargeRegulatedMotor(MotorPort.C);
 		c.rotate(-180);
 		
 		b.rotate(310);
